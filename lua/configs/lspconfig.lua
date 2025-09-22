97c3e1b9490865f15c3bcb9e3204197470aa44b6
Soham Pramanik <spramanik@tsavoritesi.com>
2025-09-21 22:36:48 -0700

    Verible Setup & config update for NVIM 0.11+

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,60 +1,96 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- local on_attach = require("nvchad.configs.lspconfig").on_attach
+-- local on_init = require("nvchad.configs.lspconfig").on_init
+-- local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- local lspconfig = require "lspconfig"
 
-local lspconfig = require "lspconfig"
+local lspconfig = vim.lsp.config
 
--- list of all servers configured.
-lspconfig.servers = {
-  "lua_ls",
-  "clangd",
-  "pyright",
-}
-
--- list of servers configured with default config.
-local default_servers = { "pyright" }
-
--- lsps with default config
-for _, lsp in ipairs(default_servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+-- Shared on_attach: Add keymaps and features for all LSPs
+local on_attach = function(client, bufnr)
+  local opts = { buffer = bufnr, noremap = true, silent = true }
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 end
 
-lspconfig.clangd.setup {
-  on_attach = function(client, bufnr)
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-    on_attach(client, bufnr)
-  end,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+-- Capabilities for better completion support
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-lspconfig.lua_ls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+-- Helper function for root_dir detection
+local function get_root_dir()
+  return vim.fs.root(0, { ".git", "Makefile", "CMakeLists.txt" })
+end
 
+------------------------------------------------------------
+-- LSP SERVER CONFIGS
+------------------------------------------------------------
+
+-- Lua
+lspconfig("lua_ls", {
   settings = {
     Lua = {
-      diagnostics = {
-        -- enable = false, -- Disable all diagnostics from lua_ls
-        -- globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          vim.fn.expand "$VIMRUNTIME/lua",
-          vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-          -- "${3rd}/love2d/library",
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
+      diagnostics = { globals = { "vim" } },
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
     },
   },
-}
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
+
+-- Python
+lspconfig("pyright", {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
+
+-- C/C++
+lspconfig("clangd", {
+  cmd = { "clangd", "--header-insertion=never" },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
+
+-- SystemVerilog / Verilog
+lspconfig("verible", {
+  cmd = { "verible-verilog-ls", "--rules=+line-length=length:200" },
+  filetypes = { "verilog", "systemverilog" },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
+
+-- Bash
+lspconfig("bashls", {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
+
+-- JSON
+lspconfig("jsonls", {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
+
+-- YAML
+lspconfig("yamlls", {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
+
+-- Markdown (optional)
+lspconfig("marksman", {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = get_root_dir(),
+})
--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,27 +1,34 @@
-local lspconfig = package.loaded["lspconfig"]
-
 -- List of servers to ignore during install
 local ignore_install = { "clangd", "pyright" }
 
--- Helper function to find if value is in table.
-local function table_contains(table, value)
-  for _, v in ipairs(table) do
-    if v == value then
-      return true
+-- Get all available LSP servers from mason-lspconfig
+-- local all_servers = require("mason-lspconfig").get_available_servers()
+local servers_to_install = {
+  "lua_ls",
+  "bashls",
+  "jsonls",
+  "yamlls",
+  "marksman",
+  "verible",
+}
+
+-- Filter out ignored servers
+local final_servers = {}
+for _, server in ipairs(servers_to_install) do
+  local skip = false
+  for _, ignore in ipairs(ignore_install) do
+    if server == ignore then
+      skip = true
+      break
     end
   end
-  return false
-end
-
--- Build a list of lsp servers to install minus the ignored list.
-local all_servers = {}
-for _, s in ipairs(lspconfig.servers) do
-  if not table_contains(ignore_install, s) then
-    table.insert(all_servers, s)
+  if not skip then
+    table.insert(final_servers, server)
   end
 end
 
+-- Setup Mason with filtered list
 require("mason-lspconfig").setup {
-  ensure_installed = all_servers,
+  ensure_installed = final_servers,
   automatic_installation = false,
 }
