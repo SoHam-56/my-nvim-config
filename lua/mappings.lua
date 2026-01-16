07ed634b424fa4430491d50be45b8463224f90ea
Soham Pramanik <spramanik@tsavoritesi.com>
2026-01-16 01:47:21 -0800

    nvim tree width change shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Resize window using <Ctrl> + Arrow Keys
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
